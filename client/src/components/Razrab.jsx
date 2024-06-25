d95df8d19ca0ac468ac3c849005284d677024ce1
alena1942 <atrifonova953@gmail.com>
2024-06-25 20:44:38 +0200

    еще кое-что

--- client/src/components/Razrab.jsx
+++ client/src/components/Razrab.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Razrab = () => {
   return (
-    <h1 className='head-text1'>
-                       Разработки
-                    </h1>
+    <div>
+      <div className='dbbs'><h1 className='head-text1'>Разработки<h3>.</h3></h1><div className='linebb'><br></br></div></div>   
+      <div>jjjjj</div>
+    </div>
   )
 }
 
