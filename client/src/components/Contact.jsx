ccc7a5cedc95634edc40d0638cfd2b6bb8025803
alena1942 <atrifonova953@gmail.com>
2024-06-25 20:42:18 +0200

    все что я сд

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <h1 className='head-text1'>
-                       Контакты
-                    </h1>
+    <div>
+      <div className='dbbs'><h1 className='head-text1'>Контакты<h3>.</h3></h1></div>  
+      <div className='foter'>
+        <div className='cont1'>
+          <div className='cont'>Контакты:</div>
+          <b className='cont'>E-mail: bocha083@mail.ru</b>
+          <b className='cont'>Тел: +7 (911) 858 95 63 </b>
+        </div>  
+        <div className='carta'> 
+        
+      </div>
+      </div>
+      </div>
   )
 }
 
