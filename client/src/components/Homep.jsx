4faf3b44072d0824f497e1d1a061b29b18a6ece7
alena1942 <atrifonova953@gmail.com>
2024-07-07 21:55:22 +0200

    svg

--- client/src/components/Homep.jsx
+++ client/src/components/Homep.jsx
@@ -19,7 +19,17 @@ import {
                         Лаборатория мобильных роботов 
                     </h1>
                 </motion.div>
-                
+                <div className='mach_t'>
+              <div className="h-36 sm:h-36 xl:h-36 2xl:h-96">
+                <Carousel>
+                    <img className='mach1t' src="/машина2.png" alt="..." />
+                    <img className='mach1t' src="/машина3.png" alt="..." />
+                    <img className='mach1t' src="/машина4.png" alt="..."/>
+                    <img className='mach1t' src='/машина5.png' alt="..."/>
+                    <img className='mach1t' src='/машина6.png' alt="..."/>
+                </Carousel>
+                </div>
+                </div>
                 <div className='fsh_t'>
                           <img src="/fsh.png" />
                 </div>
@@ -54,16 +64,16 @@ import {
                   </div>
                   </div>
             </MediaQuery>
-            <MediaQuery minWidth={1200}>
+            <MediaQuery minWidth={1200} maxWidth={1800}>
 
             <div className='g'>
                 
               <motion.div {...headTextAnimation}>
-                  <h1 className='head-text'>
+                  <h1 className='head-text_n'>
                       Лаборатория мобильных роботов 
                   </h1>
               </motion.div>
-              <div className='mach'>
+              <div className='mach_n'>
               <div className="h-36 sm:h-36 xl:h-36 2xl:h-96">
                 <Carousel>
                     <img className='mach1' src="/машина2.png" alt="..." />
@@ -102,6 +112,54 @@ import {
                 </div>
             </div>
             </MediaQuery>
+            <MediaQuery minWidth={1801} maxWidth={3980}>
+
+                <div className='g'>
+                    
+                <motion.div {...headTextAnimation}>
+                    <h1 className='head-text'>
+                        Лаборатория мобильных роботов 
+                    </h1>
+                </motion.div>
+                <div className='mach'>
+                <div className="h-36 sm:h-36 xl:h-36 2xl:h-96">
+                    <Carousel>
+                        <img className='mach1' src="/машина2.png" alt="..." />
+                        <img className='mach1' src="/машина3.png" alt="..." />
+                        <img className='mach1' src="/машина4.png" alt="..."/>
+                        <img className='mach1' src='/машина5.png' alt="..."/>
+                        <img className='mach1' src='/машина6.png' alt="..."/>
+                    </Carousel>
+                    </div>
+                    </div>
+                <div className='fsh'>
+                            <img src="/fsh.png" />
+                </div>
+                
+                </div>
+                    
+                <div className='imghead'>
+                    <div className='boltkr'>
+                        <div className='kr'>
+                            <img src='/kr.png' />
+                        </div>
+                        <div className='bolt'>
+                            <img  src="/bolt.png" /> 
+                        </div>
+                    </div>
+                    <div className='pr'>
+                        <img src="/pr.png" />
+                    </div>
+                    <div className='zakr'>
+                        <div className='zak'>
+                            <img src='/zak.png' />
+                        </div>
+                        <div className='fkr'>
+                            <img src='/fkr.png' />
+                        </div>
+                    </div>
+                </div>
+                </MediaQuery>
           </AnimatePresence>  
             
     )
